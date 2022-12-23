b9015e05cc7fcf7a0374697e74e67da9a11d6f5b
Dhiraj Kapse <kapsedhiraj15@gmail.com>
2022-12-23 21:41:25 +0530

    Added Functionality in Boost tab

--- AWS - Awesome Windows Speed/Form1.Designer.cs
+++ AWS - Awesome Windows Speed/Form1.Designer.cs
@@ -33,10 +33,17 @@
             this.materialTabControl1 = new MaterialSkin.Controls.MaterialTabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.materialSwitch5 = new MaterialSkin.Controls.MaterialSwitch();
+            this.materialSwitch4 = new MaterialSkin.Controls.MaterialSwitch();
+            this.materialSwitch3 = new MaterialSkin.Controls.MaterialSwitch();
+            this.materialSwitch2 = new MaterialSkin.Controls.MaterialSwitch();
+            this.materialSwitch1 = new MaterialSkin.Controls.MaterialSwitch();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.materialSwitch6 = new MaterialSkin.Controls.MaterialSwitch();
             this.materialTabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // materialTabControl1
@@ -70,6 +77,12 @@
             // tabPage2
             // 
             this.tabPage2.BackColor = System.Drawing.Color.White;
+            this.tabPage2.Controls.Add(this.materialSwitch6);
+            this.tabPage2.Controls.Add(this.materialSwitch5);
+            this.tabPage2.Controls.Add(this.materialSwitch4);
+            this.tabPage2.Controls.Add(this.materialSwitch3);
+            this.tabPage2.Controls.Add(this.materialSwitch2);
+            this.tabPage2.Controls.Add(this.materialSwitch1);
             this.tabPage2.ImageKey = "icons8-rocket-50.png";
             this.tabPage2.Location = new System.Drawing.Point(4, 39);
             this.tabPage2.Name = "tabPage2";
@@ -78,15 +91,85 @@
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Boost Tools";
             // 
-            // imageList1
+            // materialSwitch5
             // 
-            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
-            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
-            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
-            this.imageList1.Images.SetKeyName(0, "icons8-developer-50.png");
-            this.imageList1.Images.SetKeyName(1, "icons8-home-page-50.png");
-            this.imageList1.Images.SetKeyName(2, "icons8-remove-50.png");
-            this.imageList1.Images.SetKeyName(3, "icons8-rocket-50.png");
+            this.materialSwitch5.AutoSize = true;
+            this.materialSwitch5.Depth = 0;
+            this.materialSwitch5.Location = new System.Drawing.Point(29, 207);
+            this.materialSwitch5.Margin = new System.Windows.Forms.Padding(0);
+            this.materialSwitch5.MouseLocation = new System.Drawing.Point(-1, -1);
+            this.materialSwitch5.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialSwitch5.Name = "materialSwitch5";
+            this.materialSwitch5.Ripple = true;
+            this.materialSwitch5.Size = new System.Drawing.Size(424, 37);
+            this.materialSwitch5.TabIndex = 4;
+            this.materialSwitch5.Text = "Improve System responsiveness and Network speed";
+            this.materialSwitch5.UseVisualStyleBackColor = true;
+            this.materialSwitch5.CheckedChanged += new System.EventHandler(this.materialSwitch5_CheckedChanged);
+            // 
+            // materialSwitch4
+            // 
+            this.materialSwitch4.AutoSize = true;
+            this.materialSwitch4.Depth = 0;
+            this.materialSwitch4.Location = new System.Drawing.Point(29, 170);
+            this.materialSwitch4.Margin = new System.Windows.Forms.Padding(0);
+            this.materialSwitch4.MouseLocation = new System.Drawing.Point(-1, -1);
+            this.materialSwitch4.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialSwitch4.Name = "materialSwitch4";
+            this.materialSwitch4.Ripple = true;
+            this.materialSwitch4.Size = new System.Drawing.Size(242, 37);
+            this.materialSwitch4.TabIndex = 3;
+            this.materialSwitch4.Text = "Speedup Shut Down Time";
+            this.materialSwitch4.UseVisualStyleBackColor = true;
+            this.materialSwitch4.CheckedChanged += new System.EventHandler(this.materialSwitch4_CheckedChanged);
+            // 
+            // materialSwitch3
+            // 
+            this.materialSwitch3.AutoSize = true;
+            this.materialSwitch3.Depth = 0;
+            this.materialSwitch3.Location = new System.Drawing.Point(29, 133);
+            this.materialSwitch3.Margin = new System.Windows.Forms.Padding(0);
+            this.materialSwitch3.MouseLocation = new System.Drawing.Point(-1, -1);
+            this.materialSwitch3.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialSwitch3.Name = "materialSwitch3";
+            this.materialSwitch3.Ripple = true;
+            this.materialSwitch3.Size = new System.Drawing.Size(505, 37);
+            this.materialSwitch3.TabIndex = 2;
+            this.materialSwitch3.Text = "Slightly Improves RAM management and Overall system speed.";
+            this.materialSwitch3.UseVisualStyleBackColor = true;
+            this.materialSwitch3.CheckedChanged += new System.EventHandler(this.materialSwitch3_CheckedChanged);
+            // 
+            // materialSwitch2
+            // 
+            this.materialSwitch2.AutoSize = true;
+            this.materialSwitch2.Depth = 0;
+            this.materialSwitch2.Location = new System.Drawing.Point(29, 96);
+            this.materialSwitch2.Margin = new System.Windows.Forms.Padding(0);
+            this.materialSwitch2.MouseLocation = new System.Drawing.Point(-1, -1);
+            this.materialSwitch2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialSwitch2.Name = "materialSwitch2";
+            this.materialSwitch2.Ripple = true;
+            this.materialSwitch2.Size = new System.Drawing.Size(289, 37);
+            this.materialSwitch2.TabIndex = 1;
+            this.materialSwitch2.Text = "Disable GameDVR and GameBar";
+            this.materialSwitch2.UseVisualStyleBackColor = true;
+            this.materialSwitch2.CheckedChanged += new System.EventHandler(this.materialSwitch2_CheckedChanged);
+            // 
+            // materialSwitch1
+            // 
+            this.materialSwitch1.AutoSize = true;
+            this.materialSwitch1.Depth = 0;
+            this.materialSwitch1.Location = new System.Drawing.Point(29, 59);
+            this.materialSwitch1.Margin = new System.Windows.Forms.Padding(0);
+            this.materialSwitch1.MouseLocation = new System.Drawing.Point(-1, -1);
+            this.materialSwitch1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialSwitch1.Name = "materialSwitch1";
+            this.materialSwitch1.Ripple = true;
+            this.materialSwitch1.Size = new System.Drawing.Size(135, 37);
+            this.materialSwitch1.TabIndex = 0;
+            this.materialSwitch1.Text = "Dark Mode";
+            this.materialSwitch1.UseVisualStyleBackColor = true;
+            this.materialSwitch1.CheckedChanged += new System.EventHandler(this.materialSwitch1_CheckedChanged);
             // 
             // tabPage3
             // 
@@ -108,6 +191,31 @@
             this.tabPage4.TabIndex = 3;
             this.tabPage4.Text = "About";
             // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "icons8-developer-50.png");
+            this.imageList1.Images.SetKeyName(1, "icons8-home-page-50.png");
+            this.imageList1.Images.SetKeyName(2, "icons8-remove-50.png");
+            this.imageList1.Images.SetKeyName(3, "icons8-rocket-50.png");
+            // 
+            // materialSwitch6
+            // 
+            this.materialSwitch6.AutoSize = true;
+            this.materialSwitch6.Depth = 0;
+            this.materialSwitch6.Location = new System.Drawing.Point(29, 22);
+            this.materialSwitch6.Margin = new System.Windows.Forms.Padding(0);
+            this.materialSwitch6.MouseLocation = new System.Drawing.Point(-1, -1);
+            this.materialSwitch6.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialSwitch6.Name = "materialSwitch6";
+            this.materialSwitch6.Ripple = true;
+            this.materialSwitch6.Size = new System.Drawing.Size(271, 37);
+            this.materialSwitch6.TabIndex = 5;
+            this.materialSwitch6.Text = "Ultimate Performance Tweaks";
+            this.materialSwitch6.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
@@ -120,6 +228,8 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "AWS - Awesome Windows Speed";
             this.materialTabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -132,5 +242,11 @@
         private ImageList imageList1;
         private TabPage tabPage3;
         private TabPage tabPage4;
+        private MaterialSkin.Controls.MaterialSwitch materialSwitch5;
+        private MaterialSkin.Controls.MaterialSwitch materialSwitch4;
+        private MaterialSkin.Controls.MaterialSwitch materialSwitch3;
+        private MaterialSkin.Controls.MaterialSwitch materialSwitch2;
+        private MaterialSkin.Controls.MaterialSwitch materialSwitch1;
+        private MaterialSkin.Controls.MaterialSwitch materialSwitch6;
     }
 }
--- AWS - Awesome Windows Speed/Form1.cs
+++ AWS - Awesome Windows Speed/Form1.cs
@@ -1,4 +1,12 @@
 using MaterialSkin.Controls;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
 
 namespace AWS___Awesome_Windows_Speed
 {
@@ -13,6 +21,353 @@ namespace AWS___Awesome_Windows_Speed
             materialSkinManager.AddFormToManage(this);
             materialSkinManager.Theme = MaterialSkin.MaterialSkinManager.Themes.LIGHT;
             materialSkinManager.ColorScheme = new MaterialSkin.ColorScheme(MaterialSkin.Primary.Indigo500, MaterialSkin.Primary.Indigo700, MaterialSkin.Primary.Indigo100,MaterialSkin.Accent.Pink200,MaterialSkin.TextShade.WHITE);
+        }
+
+        private void materialSwitch1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (materialSwitch1.Checked)
+            {
+                // Create a temporary batch file
+                string tempFile = Path.GetTempFileName() + ".bat";
+
+                // Write the batch script to the temporary file
+                File.WriteAllText(tempFile, @"
+                @echo off
+
+                rem Enable dark mode in Windows
+                reg add ""HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize"" /v AppsUseLightTheme /t REG_DWORD /d 0 /f
+
+                rem Set the color scheme to dark
+                reg add ""HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize"" /v SystemUsesLightTheme /t REG_DWORD /d 0 /f
+
+                rem Set the default app mode to dark
+                reg add ""HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize"" /v DefaultAppMode /t REG_DWORD /d 1 /f
+                ");
+
+                // Create a new ProcessStartInfo object
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = "/c " + tempFile;
+                startInfo.Verb = "runas";
+
+                // Start the Command Prompt as an administrator and run the batch script
+                Process.Start(startInfo);
+            }
+            else 
+            {
+                // Create a temporary batch file
+                string tempFile = Path.GetTempFileName() + ".bat";
+
+                // Write the batch script to the temporary file
+                File.WriteAllText(tempFile, @"
+                @echo off
+
+                rem Disable dark mode in Windows
+                reg add ""HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize"" /v AppsUseLightTheme /t REG_DWORD /d 1 /f
+
+                rem Set the color scheme to light
+                reg add ""HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize"" /v SystemUsesLightTheme /t REG_DWORD /d 1 /f
+
+                rem Set the default app mode to light
+                reg add ""HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize"" /v DefaultAppMode /t REG_DWORD /d 0 /f
+                ");
+
+                // Create a new ProcessStartInfo object
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = "/c " + tempFile;
+                startInfo.Verb = "runas";
+
+                // Start the Command Prompt as an administrator and run the batch script
+                Process.Start(startInfo);
+            }
+        }
+
+        private void materialSwitch2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (materialSwitch2.Checked)
+            {
+                // Create a temporary batch file
+                string tempFile = Path.GetTempFileName() + ".bat";
+
+                // Write the batch script to the temporary file
+                File.WriteAllText(tempFile, @"
+                @echo off
+
+                rem Add the ""AllowGameDVR"" value
+                echo Adding ""AllowGameDVR"" value...
+                reg add ""HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\PolicyManager\default\ApplicationManagement"" /v ""AllowGameDVR"" /t REG_DWORD /d 00000000 /f
+
+                rem Add the ""GameDVR_Enabled"" value
+                echo Adding ""GameDVR_Enabled"" value...
+                reg add ""HKEY_CURRENT_USER\System\GameConfigStore"" /v ""GameDVR_Enabled"" /t REG_DWORD /d 0 /f
+
+                rem Add the ""GameDVR_FSEBehavior"" value
+                echo Adding ""GameDVR_FSEBehavior"" value...
+                reg add ""HKEY_CURRENT_USER\System\GameConfigStore"" /v ""GameDVR_FSEBehavior"" /t REG_DWORD /d 00000002 /f
+
+                rem Add the ""GameDVR_FSEBehaviorMode"" value
+                echo Adding ""GameDVR_FSEBehaviorMode"" value...
+                reg add ""HKEY_CURRENT_USER\System\GameConfigStore"" /v ""GameDVR_FSEBehaviorMode"" /t REG_DWORD /d 00000002 /f
+
+                rem Add the ""AllowGameDVR"" value
+                echo Adding ""AllowGameDVR"" value...
+                reg add ""HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\GameDVR"" /v ""AllowGameDVR"" /t REG_DWORD /d 0 /f
+                echo Disabled GameDVR and GameBar Successfully!
+
+                pause
+                ");
+
+                // Create a new ProcessStartInfo object
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = "/c " + tempFile;
+                startInfo.Verb = "runas";
+
+                // Start the Command Prompt as an administrator and run the batch script
+                Process.Start(startInfo);
+            }
+            else
+            {
+                // Create a temporary batch file
+                string tempFile = Path.GetTempFileName() + ".bat";
+
+                // Write the batch script to the temporary file
+                File.WriteAllText(tempFile, @"
+                @echo off
+                
+                rem Delete the ""AllowGameDVR"" value
+                echo Deleting ""AllowGameDVR"" value...
+                reg delete ""HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\PolicyManager\default\ApplicationManagement"" /v ""AllowGameDVR"" /f
+
+                rem Delete the ""GameDVR_Enabled"" value
+                echo Deleting ""GameDVR_Enabled"" value...
+                reg delete ""HKEY_CURRENT_USER\System\GameConfigStore"" /v ""GameDVR_Enabled"" /f
+
+                rem Delete the ""GameDVR_FSEBehavior"" value
+                echo Deleting ""GameDVR_FSEBehavior"" value...
+                reg delete ""HKEY_CURRENT_USER\System\GameConfigStore"" /v ""GameDVR_FSEBehavior"" /f
+
+                rem Delete the ""GameDVR_FSEBehaviorMode"" value
+                echo Deleting ""GameDVR_FSEBehaviorMode"" value...
+                reg delete ""HKEY_CURRENT_USER\System\GameConfigStore"" /v ""GameDVR_FSEBehaviorMode"" /f
+
+                rem Delete the ""AllowGameDVR"" value
+                echo Deleting ""AllowGameDVR"" value...
+                reg delete ""HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\GameDVR"" /v ""AllowGameDVR"" /f
+                echo Successfully Enabled GameDVR and GameBar!
+
+                pause
+
+                ");
+
+                // Create a new ProcessStartInfo object
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = "/c " + tempFile;
+                startInfo.Verb = "runas";
+
+                // Start the Command Prompt as an administrator and run the batch script
+                Process.Start(startInfo);
+            }
+        }
+
+        private void materialSwitch3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (materialSwitch3.Checked)
+            {
+                // Create a temporary batch file
+                string tempFile = Path.GetTempFileName() + ".bat";
+
+                // Write the batch script to the temporary file
+                File.WriteAllText(tempFile, @"
+                @echo off
+
+                rem Add the ""AutoEndTasks"" value
+                echo Adding ""AutoEndTasks"" value...
+                reg add ""HKEY_CURRENT_USER\Control Panel\Desktop"" /v ""AutoEndTasks"" /t REG_SZ /d ""1"" /f
+
+                rem Add the ""HungAppTimeout"" value
+                echo Adding ""HungAppTimeout"" value...
+                reg add ""HKEY_CURRENT_USER\Control Panel\Desktop"" /v ""HungAppTimeout"" /t REG_SZ /d ""1000"" /f
+
+                rem Add the ""MenuShowDelay"" value
+                echo Adding ""MenuShowDelay"" value...
+                reg add ""HKEY_CURRENT_USER\Control Panel\Desktop"" /v ""MenuShowDelay"" /t REG_SZ /d ""8"" /f
+
+                rem Add the ""WaitToKillAppTimeout"" value
+                echo Adding ""WaitToKillAppTimeout"" value...
+                reg add ""HKEY_CURRENT_USER\Control Panel\Desktop"" /v ""WaitToKillAppTimeout"" /t REG_SZ /d ""2000"" /f
+
+                rem Add the ""LowLevelHooksTimeout"" value
+                echo Adding ""LowLevelHooksTimeout"" value...
+                reg add ""HKEY_CURRENT_USER\Control Panel\Desktop"" /v ""LowLevelHooksTimeout"" /t REG_SZ /d ""1000"" /f
+                echo Done!
+
+                pause
+
+
+                ");
+
+                // Create a new ProcessStartInfo object
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = "/c " + tempFile;
+                startInfo.Verb = "runas";
+
+                // Start the Command Prompt as an administrator and run the batch script
+                Process.Start(startInfo);
+            }
+            else 
+            {
+                // Create a temporary batch file
+                string tempFile = Path.GetTempFileName() + ".bat";
+
+                // Write the batch script to the temporary file
+                File.WriteAllText(tempFile, @"
+                @echo off
+
+                rem Delete the ""AutoEndTasks"" value
+                echo Deleting ""AutoEndTasks"" value...
+                reg delete ""HKEY_CURRENT_USER\Control Panel\Desktop"" /v ""AutoEndTasks"" /f
+
+                rem Delete the ""HungAppTimeout"" value
+                echo Deleting ""HungAppTimeout"" value...
+                reg delete ""HKEY_CURRENT_USER\Control Panel\Desktop"" /v ""HungAppTimeout"" /f
+
+                rem Delete the ""MenuShowDelay"" value
+                echo Deleting ""MenuShowDelay"" value...
+                reg delete ""HKEY_CURRENT_USER\Control Panel\Desktop"" /v ""MenuShowDelay"" /f
+
+                rem Delete the ""WaitToKillAppTimeout"" value
+                echo Deleting ""WaitToKillAppTimeout"" value...
+                reg delete ""HKEY_CURRENT_USER\Control Panel\Desktop"" /v ""WaitToKillAppTimeout"" /f
+
+                rem Delete the ""LowLevelHooksTimeout"" value
+                echo Deleting ""LowLevelHooksTimeout"" value...
+                reg delete ""HKEY_CURRENT_USER\Control Panel\Desktop"" /v ""LowLevelHooksTimeout"" /f
+                echo Done!
+
+                pause
+                ");
+
+                // Create a new ProcessStartInfo object
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = "/c " + tempFile;
+                startInfo.Verb = "runas";
+
+                // Start the Command Prompt as an administrator and run the batch script
+                Process.Start(startInfo);
+            }
+        }
+
+        private void materialSwitch4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (materialSwitch4.Checked)
+            {
+                string tempFile = Path.GetTempFileName() + ".bat";
+
+                // Write the batch script to the temporary file
+                File.WriteAllText(tempFile, @"
+                @echo off
+
+                rem Add the ""WaitToKillServiceTimeout"" value
+                echo Adding ""WaitToKillServiceTimeout"" value...
+                reg add ""HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control"" /v ""WaitToKillServiceTimeout"" /t REG_SZ /d ""2000"" /f
+                echo Done!
+
+                pause
+                ");
+
+                // Create a new ProcessStartInfo object
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = "/c " + tempFile;
+                startInfo.Verb = "runas";
+
+                // Start the Command Prompt as an administrator and run the batch script
+                Process.Start(startInfo);
+            }
+            else 
+            {
+                string tempFile = Path.GetTempFileName() + ".bat";
+
+                // Write the batch script to the temporary file
+                File.WriteAllText(tempFile, @"
+                @echo off
+
+                rem Delete the ""WaitToKillServiceTimeout"" value
+                echo Deleting ""WaitToKillServiceTimeout"" value...
+                reg delete ""HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control"" /v ""WaitToKillServiceTimeout"" /f
+                echo Done!
+
+                pause
+                ");
+
+                // Create a new ProcessStartInfo object
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = "/c " + tempFile;
+                startInfo.Verb = "runas";
+
+                // Start the Command Prompt as an administrator and run the batch script
+                Process.Start(startInfo);
+            }
+        }
+
+        private void materialSwitch5_CheckedChanged(object sender, EventArgs e)
+        {   
+            if (materialSwitch5.Checked)
+            {
+                string tempFile = Path.GetTempFileName() + ".bat";
+
+                // Write the batch script to the temporary file
+                File.WriteAllText(tempFile, @"
+                @echo off
+
+                rem Add the ""WaitToKillServiceTimeout"" value
+                echo Adding ""WaitToKillServiceTimeout"" value...
+                reg add ""HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control"" /v ""WaitToKillServiceTimeout"" /t REG_SZ /d ""2000"" /f
+                echo Done!
+
+                pause
+                ");
+
+                // Create a new ProcessStartInfo object
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = "/c " + tempFile;
+                startInfo.Verb = "runas";
+
+                // Start the Command Prompt as an administrator and run the batch script
+                Process.Start(startInfo);
+            }
+            else
+            {
+                string tempFile = Path.GetTempFileName() + ".bat";
+
+                // Write the batch script to the temporary file
+                File.WriteAllText(tempFile, @"
+                @echo off
+
+                rem Delete the ""WaitToKillServiceTimeout"" value
+                echo Deleting ""WaitToKillServiceTimeout"" value...
+                reg delete ""HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control"" /v ""WaitToKillServiceTimeout"" /f
+                echo Done!
+
+                pause
+                ");
+
+                // Create a new ProcessStartInfo object
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = "/c " + tempFile;
+                startInfo.Verb = "runas";
+
+                // Start the Command Prompt as an administrator and run the batch script
+                Process.Start(startInfo);
+            }
         }
     }
 }
